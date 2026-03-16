caa85c2afa30163c3f3e083041e2a11f511c7018
Touy <touy_smith@hotmail.com>
2026-03-16 10:01:27 -0700

    dokploy

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/driverssystem",
+  output: "standalone",
 };
 
 export default nextConfig;
